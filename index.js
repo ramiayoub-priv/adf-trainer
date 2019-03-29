5075d0e249f420d8751f6f3b5cdcb566e3a96bb4
Rami Ayoub <ramiayoub@gmail.com>
2019-03-29 17:29:39 +0200

    adf compass rose and needles rotate

---
+++ index.js
@@ -0,0 +1,124 @@
+function setpixelated(context){
+    context['imageSmoothingEnabled'] = false;       /* standard */
+    context['mozImageSmoothingEnabled'] = false;    /* Firefox */
+    context['oImageSmoothingEnabled'] = false;      /* Opera */
+    context['webkitImageSmoothingEnabled'] = false; /* Safari */
+    context['msImageSmoothingEnabled'] = false;     /* IE */
+}
+
+var c = document.getElementById("myCanvas");
+var canvasDiv = document.getElementById("canvasCol");
+var ctx = c.getContext("2d");
+
+window.addEventListener("resize", resizeCanvas);
+
+var canvasInternalSizeX = 500;
+var canvasInternalSizeY = 500;
+
+reDraw(1,1);
+
+function resizeCanvas() {
+    
+    var bb = canvasDiv.getBoundingClientRect(),
+    width = bb.right - bb.left;
+    var heightRatio = 1;
+    c.width = width;
+    c.height = c.width * heightRatio;
+    let scaleX = c.width / canvasInternalSizeX;
+    let scaleY = c.height / canvasInternalSizeY;
+    reDraw(scaleX,scaleY);
+  }
+
+
+
+function reDraw(scaleX,scaleY) {
+    
+    ctx.scale(scaleX,scaleY);
+    setpixelated(ctx);
+    ctx.moveTo(0,0);
+    render();
+
+    
+    
+}
+var prevRot = 0;
+var prevRot2 = 0;
+
+var adfBase = new Image();
+adfBase.src = 'img/adf_base.png';
+
+var adfRose = new Image();
+adfRose.src = 'img/adf_rose_black.png';
+
+var adfNeedle = new Image();
+adfNeedle.src = 'img/adf_needle.png';
+
+adfBase.onload = function(){ 
+    render();
+}
+
+adfRose.onload = function(){ 
+    render();
+}
+
+adfNeedle.onload = function(){ 
+    render();
+}
+
+function render() {
+
+
+    if(adfBase && adfNeedle && adfRose) {
+        ctx.clearRect(0, 0, c.width, c.height);   
+        
+    
+    //adfRose.onload = function(){
+        
+
+        ctx.save();
+        ctx.translate(500/2,500/2);
+        ctx.rotate(prevRot2*Math.PI/180);
+        
+        ctx.translate(-500/2,-500/2);
+        ctx.drawImage(adfRose, 0, 0, 500, (adfRose.height / adfRose.width) * 500);
+        ctx.restore();
+        prevRot2 = prevRot2+10;
+        if(prevRot2 == 360) {
+            prevRot2 = 0;
+        }
+    //}
+
+  
+    //adfNeedle.onload = function(){
+        console.log(adfNeedle.width + " - " + adfNeedle.height);
+        ctx.save();
+        ctx.translate(500/2,500/2);
+        ctx.rotate(prevRot*Math.PI/180);
+        
+        ctx.translate(-500/2,-500/2);
+        ctx.drawImage(adfNeedle, 100, 100, 300, (adfNeedle.height / adfNeedle.width) * 300);
+        ctx.restore();
+        prevRot = prevRot+1;
+        if(prevRot == 360) {
+            prevRot = 0;
+        }
+
+    
+    ctx.drawImage(adfBase, 0, 0, 500, (adfBase.height / adfBase.width) * 500);
+
+        
+    }
+
+    
+
+        
+    //}
+
+   
+}
+
+setInterval(render,1000);
+
+
+
+
